dd3c846695a676e7d9b59c2329e6374ad94b707b
Wes Johnson <wes@wes-johnson.com>
2026-04-20 13:08:08 +0200

    Make station code configurable; fix gust parsing and matching bug

--- gustSpeed.py
+++ gustSpeed.py
@@ -1,9 +1,13 @@
 import json
+import os
+import sys
 import urllib.request
 #links https://gist.github.com/philshem/0812d610f11d26a042bbd3e617fa8d22?short_path=bb4816a
 #file = 'https://data.geo.admin.ch/ch.meteoschweiz.messwerte-niederschlag-10min/ch.meteoschweiz.messwerte-niederschlag-10min_en.json'
 
-location = "REH"
+location_code = sys.argv[1] if len(sys.argv) > 1 else os.environ.get("METEOSWISS_STATION")
+if not location_code:
+    sys.exit("Usage: gustSpeed.py <station_code>  (or set METEOSWISS_STATION)")
 
 with urllib.request.urlopen('https://data.geo.admin.ch/ch.meteoschweiz.messwerte-wind-boeenspitze-kmh-10min/ch.meteoschweiz.messwerte-wind-boeenspitze-kmh-10min_en.json') as url:
         json_data = json.loads(url.read().decode())
@@ -11,8 +15,7 @@ with urllib.request.urlopen('https://data.geo.admin.ch/ch.meteoschweiz.messwerte
 locations = json_data["features"]
 
 for location in locations:
-    if location["id"] == location:
+    if location["id"] == location_code:
         data = location["properties"]["description"]
-        wind_speed = data.split("Gust peak</strong></td><td>")[1]
-        wind_speed_data = wind_speed[0] + wind_speed[1] + wind_speed[2]
+        wind_speed_data = data.split("Gust peak</strong></td><td>")[1].split("</td>")[0]
         print(wind_speed_data)
--- precipitation.py
+++ precipitation.py
@@ -1,9 +1,13 @@
 import json
+import os
+import sys
 import urllib.request
 #links https://gist.github.com/philshem/0812d610f11d26a042bbd3e617fa8d22?short_path=bb4816a
 #file = 'https://data.geo.admin.ch/ch.meteoschweiz.messwerte-niederschlag-10min/ch.meteoschweiz.messwerte-niederschlag-10min_en.json'
 
-location_code = ""
+location_code = sys.argv[1] if len(sys.argv) > 1 else os.environ.get("METEOSWISS_STATION")
+if not location_code:
+    sys.exit("Usage: precipitation.py <station_code>  (or set METEOSWISS_STATION)")
 
 with urllib.request.urlopen('https://data.geo.admin.ch/ch.meteoschweiz.messwerte-niederschlag-10min/ch.meteoschweiz.messwerte-niederschlag-10min_en.json') as url:
         json_data = json.loads(url.read().decode())
@@ -13,6 +17,5 @@ locations = json_data["features"]
 for location in locations:
         if location["id"] == location_code:
                 data = location["properties"]["description"]
-                Precipitation = data.split("Precipitation</strong></td><td>")[1]
-                Precipitation_data = Precipitation[0] + Precipitation[1] + Precipitation[2]
+                Precipitation_data = data.split("Precipitation</strong></td><td>")[1].split("</td>")[0]
                 print(Precipitation_data)
